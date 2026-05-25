5747419fa942c6f1e61258800824b8b944a551e7
dudedp <dineshprabudp1998@gmail.com>
2026-05-25 10:29:47 +0530

    stage_19 [cleanup]

--- test_csv3.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-csv_url = "https://assets.upstox.com/market-quote/instruments/exchange/complete.csv.gz"
-df = pd.read_csv(csv_url)
-print("Columns:", df.columns.tolist())
-print(df[df['name'] == 'NIFTY'].head(2).T)
--- test_db.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-from supabase import create_client
-
-load_dotenv()
-supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
-print(supabase.table('telemetry').select('*').eq('id', 1).execute())
-print(supabase.table('bot_control').select('*').eq('id', 1).execute())
